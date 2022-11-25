a5d38a5d80d581d0cd6b4d1aef250d89a68ce3e5
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-25 16:11:15 +0500

    css changes in header

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -1,10 +1,25 @@
 <template>
-  <header class="header">usman ahmed</header>
+  <header class="header">
+    <div class="logo">
+      <LogoIcon />
+    </div>
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Careers</li>
+        <li>Events</li>
+        <li>Products</li>
+        <li>Support</li>
+      </ul>
+    </nav>
+  </header>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import LogoIcon from "@/components/icons/logoIcon.vue";
 
 export default defineComponent({
+  components: { LogoIcon },
   setup() {
     return {};
   },
