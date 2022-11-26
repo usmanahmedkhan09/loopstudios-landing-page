6f3f3ef7d6e08f05484dae56b2ac75234c0c9bb8
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-26 23:45:20 +0500

    changes

--- src/components/layout/sidebar.vue
+++ src/components/layout/sidebar.vue
@@ -4,13 +4,26 @@
     :class="sidebar ? 'open' : ''"
     @click="$emit('closeSidebar')"
   >
-    <div class="sidenav"></div>
+    <div class="sidenav">
+      <div class="cross_icon">
+        <CloseIconVue />
+      </div>
+      <ul>
+        <li><a href="">About</a></li>
+        <li><a href="">Careers</a></li>
+        <li><a href="">Events</a></li>
+        <li><a href="">Products</a></li>
+        <li><a href="">Support</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import CloseIconVue from "../icons/CloseIcon.vue";
 
 export default defineComponent({
+  components: { CloseIconVue },
   props: {
     sidebar: {
       type: Boolean,
