c0a1a84edd042bd27c192927dad04aaf660bb83d
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-26 23:27:37 +0500

    changes in css

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -13,22 +13,31 @@
           <li>Support</li>
         </ul>
       </nav>
-      <MenuVue class="menu_icon" />
+      <MenuVue @click="sidebar = true" class="menu_icon" />
     </div>
     <div class="heading">
       <h1>IMMERSIVE EXPERIENCES THAT DELIVER</h1>
     </div>
+    <SidebarVue
+      :sidebar="sidebar"
+      v-if="sidebar"
+      @closeSidebar="sidebar = false"
+    />
   </header>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import LogoIcon from "@/components/icons/logoIcon.vue";
 import MenuVue from "../icons/menu.vue";
+import SidebarVue from "./sidebar.vue";
 
 export default defineComponent({
-  components: { LogoIcon, MenuVue },
+  components: { LogoIcon, MenuVue, SidebarVue },
   setup() {
-    return {};
+    const sidebar = ref(false);
+    return {
+      sidebar,
+    };
   },
 });
 </script>
---
+++ src/components/layout/sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="sidebar"
+    :class="sidebar ? 'open' : ''"
+    @click="$emit('closeSidebar')"
+  >
+    <div class="sidenav"></div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    sidebar: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
