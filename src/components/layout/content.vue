7a32c10d916239b22859b34701b1b501ecd6511b
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-27 12:36:43 +0500

    content added

---
+++ src/components/layout/content.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="content">
+    <div class="section--one">
+      <div class="image_wrapper">
+        <img src="../../assets/images/desktop/image-interactive.jpg" alt="" />
+      </div>
+      <div class="card"></div>
+    </div>
+  </main>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,12 +1,14 @@
 <template>
   <HeaderVue />
+  <Content />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import HeaderVue from "./header.vue";
+import Content from "./content.vue";
 
 export default defineComponent({
-  components: { HeaderVue },
+  components: { HeaderVue, Content },
   setup() {
     return {};
   },
