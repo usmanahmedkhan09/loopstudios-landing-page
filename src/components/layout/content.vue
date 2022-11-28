d23535f8bbcb3053e9584bed89634d601d1256de
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-29 00:27:46 +0500

    changes

--- src/components/layout/content.vue
+++ src/components/layout/content.vue
@@ -3,6 +3,15 @@
     <div class="section--one">
       <div class="image_wrapper">
         <img src="../../assets/images/desktop/image-interactive.jpg" alt="" />
+        <!-- <div class="card">
+          <h1>THE LEADER IN INTERACTIVE VR</h1>
+          <p>
+            Founded in 2011, Loopstudios has been producing world-class virtual
+            reality projects fro some of the best companies around the globe.
+            Our award-wining creations have transfromed bussiness through
+            digital experience that bind to their brand.
+          </p>
+        </div> -->
       </div>
       <div class="card">
         <h1>THE LEADER IN INTERACTIVE VR</h1>
